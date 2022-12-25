63e7afc4d6968bbd670d638c75cb2865666208ed
KenInaba <jiandaoye293@gmail.com>
2022-12-26 06:27:11 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 dir=~
+
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
@@ -11,4 +12,4 @@ timeout 10 ros2 launch CIT_DAR send_tips.launch.py > /tmp/CIT_DAR.log
 cat /tmp/CIT_DAR.log |
 grep 'ロボットシステム学'
 
-
+[ "$?" = 0 ] && echo OK!
