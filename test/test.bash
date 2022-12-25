50662938810b793a3f276b20806b75bbdaacafda
KenInaba <jiandaoye293@gmail.com>
2022-12-26 06:08:37 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -11,16 +11,4 @@ timeout 10 ros2 launch CIT_DAR send_tips.launch.py > /tmp/CIT_DAR.log
 cat /tmp/CIT_DAR.log |
 grep 'ロボットシステム学'
 
-cat /tmp/CIT_DAR.log |
-grep '信号処理論'
-
-cat /tmp/CIT_DAR.log |
-grep 'ロボットマニピレータ'
-
-cat /tmp/CIT_DAR.log |
-grep 'センサ工学'
-
-cat /tmp/CIT_DAR.log |
-grep '設計制作'
-
 
