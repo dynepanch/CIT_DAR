4c69a0f1279f74e7d620bc0236ff063407654368
KenInaba <jiandaoye293@gmail.com>
2022-12-26 06:39:22 +0900

    test

--- CIT_DAR/test/test.bash
+++ CIT_DAR/test/test.bash
@@ -6,7 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch CIT_DAR send_tips.launch.py > /tmp/CIT_DAR.log
+timeout 20 ros2 launch CIT_DAR send_tips.launch.py > /tmp/CIT_DAR.log
 
 cat /tmp/CIT_DAR.log |
 grep 'ロボットシステム学'
