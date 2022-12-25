737b81792140ab6fa1a58443766226761f3ef3c4
KenInaba <jiandaoye293@gmail.com>
2022-12-26 00:37:20 +0900

    first

---
+++ install/CIT_DAR/lib/python3.10/site-packages/CIT_DAR/send.py
@@ -0,0 +1,14 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "上田隆一":
+        response.tips = "ロボットシステム学"
+
+    return response
+
+rclpy.init()
+node = Node("send_data")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
