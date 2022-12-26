9457c5b29b63bb021f188d2668b804d59eaafbcb
KenInaba <jiandaoye293@gmail.com>
2022-12-26 11:37:11 +0900

    cds

--- CIT_DAR/test/test.bash
+++ CIT_DAR/test/test.bash
@@ -2,12 +2,15 @@
 
 dir=~
 [ "$1" != "" ] && dir="$1"
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
 
 ad=ros2 interface show person_msgs/srv/Query
 cd $dir/ros2_ws/scr
 [ "$ad" = "" ] && git clone https://github.com/dynepanch/person_msgs.git
 cd $dir/ros2_ws
-colcon build
+olcon build
 source $dir/.bashrc
 timeout 20 ros2 launch CIT_DAR send_tips.launch.py > /tmp/CIT_DAR.log
 
