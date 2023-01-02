6c26a0fda213b8375361461f4a5fb6e25d6f28b8
KenInaba <jiandaoye293@gmail.com>
2023-01-02 17:07:03 +0900

    latest

---
+++ CIT_DAR/person.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#SPDX-FileCopyrightText: 2022 Ken Inaba rightman20020920@yahoo.co,jp
+#SPDX-Licence-Identifire: BSD-3-Clause
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws/src
+git clone https://github.com/dynepanch/person_msgs.git
+colcon build
+source $dir/.bashrc
